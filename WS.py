5264bc105db9f92f2a92cf548ea6b3c894c9ecb4
Dawiden1 <dawiden1@gmail.com>
2020-11-06 20:21:36 +0100

    First Project

---
+++ WS.py
@@ -0,0 +1,9 @@
+print ("Projekt - Miernictwo i Systemy Pomiarowe \n Dawid Mądry \n Energetyka EN-2 \n Semestr 3 \n 144973")
+
+
+
+import csv
+with open('u_LED_2W.csv') as csvfile:
+    readCSV = csv.reader(csvfile)
+    for row in readCSV:
+        print(row)
