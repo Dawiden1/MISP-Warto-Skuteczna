e5c9cbc97e915b4c9b5fb261c2ba48243fca7238
Dawiden1 <dawiden1@gmail.com>
2020-11-07 16:34:58 +0100

    Witam

--- WS.py
+++ WS.py
@@ -1,10 +1,10 @@
 print ("Projekt - Miernictwo i Systemy Pomiarowe \n Dawid Mądry \n Energetyka EN-2 \n Semestr 3 \n 144973")
 
-import csv
-import matplotlib.pyplot as plt
+import csv #biblioteka obsługi plików csv
+import matplotlib.pyplot as plt #biblioteka rysowania wykresów funkcji
 
 # dane
-dane = []
+dane = [] #tabela z danymi"
 N = 500 # liczba próbek
 T = 3*N # czas trwania pomiaru
 
@@ -14,11 +14,11 @@ i = 0
 with open('u_LED_2W.csv') as csvfile:
     readCSV = csv.reader(csvfile)
 
-    for row in readCSV:
+    for row in readCSV: #zapętlanie wierszowo pliku csv
         i += 1
         if i>N:
             break
-        dane.append(float(row[0]))
+        dane.append(float(row[0])) #zamiana liczb calkowitych w tabeli na liczby zmiennoprzecinkowe
 
 #obliczanie całki z próbek
 calka = 0
@@ -33,5 +33,5 @@ Usk = (calka/T)**(1/2)
 #wyświetlanie
 print(Usk)
 
-plt.plot(dane)
-plt.show()
+plt.plot(dane) #tworzeniew wykresu
+plt.show() #wyświetlenie wykresu
