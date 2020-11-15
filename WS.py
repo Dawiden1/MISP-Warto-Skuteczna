1c4200164ed3bba41553475d2b6860c2d48fe4c5
Dawiden1 <dawiden1@gmail.com>
2020-11-15 13:14:26 +0100

    aktualizacja

--- WS.py
+++ WS.py
@@ -31,56 +31,58 @@ while odpowiedz not in ("Tak", "Nie", "nie"):
             calka += (x**2)*3
 
 #obliczanie wartości skutecznej z całki
-    Usk = (calka/T)**(1/2)
+        Usk = (calka/T)**(1/2)
 
 #wyświetlanie
-    print("Wartość Skuteczna Napięcia = ", end='')
-    print(Usk)
+        print("Wartość Skuteczna Napięcia = ", end='')
+        print(Usk)
 
-    plt.plot(dane) #tworzeniew wykresu
-    plt.show() #wyświetlenie wykresu
+        plt.plot(dane) #tworzeniew wykresu
+        plt.show() #wyświetlenie wykresu
 
-    print("Największa wartość napięcia =", max(dane))
-    print("Najmniejsza wartość napięcia =", min(dane))
+        print("Największa wartość napięcia =", max(dane))
+        print("Najmniejsza wartość napięcia =", min(dane))
 
 
 
-#
-print ("Projekt - Miernictwo i Systemy Pomiarowe \n Dawid Mądry \n Energetyka EN-2 \n Semestr 3 \n 144973")
 
-import csv #biblioteka obsługi plików csv
-import matplotlib.pyplot as plt #biblioteka rysowania wykresów funkcji
+
+
+# Stary kod
+#print ("Projekt - Miernictwo i Systemy Pomiarowe \n Dawid Mądry \n Energetyka EN-2 \n Semestr 3 \n 144973")
+
+#import csv #biblioteka obsługi plików csv
+#import matplotlib.pyplot as plt #biblioteka rysowania wykresów funkcji
 
 # dane
-dane = [] #tabela z danymi"
-N = 500 # liczba próbek
-T = 3*N # czas trwania pomiaru
+#dane = [] #tabela z danymi"
+#N = 500 # liczba próbek
+#T = 3*N # czas trwania pomiaru
 
 
 #wczytywanie N pierwszych próbek do "dane"
-i = 0
-with open('u_LED_2W.csv') as csvfile:
-    readCSV = csv.reader(csvfile)
+#i = 0
+#with open('u_LED_2W.csv') as csvfile:
+    #readCSV = csv.reader(csvfile)
 
-    for row in readCSV: #zapętlanie wierszowo pliku csv
-        i += 1
-        if i>N:
-            break
-        dane.append(float(row[0])) #zamiana liczb calkowitych w tabeli na liczby zmiennoprzecinkowe
+    #for row in readCSV: #zapętlanie wierszowo pliku csv
+        #i += 1
+        #if i>N:
+            #break
+        #dane.append(float(row[0])) #zamiana liczb calkowitych w tabeli na liczby zmiennoprzecinkowe
 
 #obliczanie całki z próbek
-calka = 0
-for x in dane:
-    calka += (x**2)*3
+#calka = 0
+#for x in dane:
+#    calka += (x**2)*3
 
 
 #obliczanie wartości skutecznej z całki
-Usk = (calka/T)**(1/2)
+#Usk = (calka/T)**(1/2)
 
 
 #wyświetlanie
-print(Usk)
+#print(Usk)
 
-plt.plot(dane) #tworzeniew wykresu
-plt.show() #wyświetlenie wykresu
-)
+#plt.plot(dane) #tworzeniew wykresu
+#plt.show() #wyświetlenie wykresu
