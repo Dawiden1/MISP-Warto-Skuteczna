857e972fb0dd286946f610dfb96e1517decc69be
Dawiden1 <dawiden1@gmail.com>
2020-11-24 18:11:25 +0100

    Kilka zmian

--- main.py
+++ main.py
@@ -38,11 +38,11 @@ def plot(data, periods = 3):
 
 def printInfo(data):
     T = basicPeriod(data)*3
-    print("Największa wartość napięcia = ", max(data), " [V]")
+    print("\nNajwiększa wartość napięcia = ", max(data), " [V]")
     print("Najmniejsza wartość napięcia = ", min(data), " [V]")
     print("Okres podstawowy = ", T, " [s]")
     print("Częstotliwość = ", 1/T, " [Hz]")
-    print("Wartosc skuteczna = ", effectiveValue(data), " [V]")
+    print("Wartosc skuteczna = ", effectiveValue(data), " [V] \n")
 
 def menu():
     print("[1] Wczytaj plik: 'u_swietlowka_12W.csv'")
@@ -57,22 +57,28 @@ dane = None
 while True:
     choice = menu()
     if choice == 0:
-        print("Dziękuje za użytkowanie programu. Z wyrazami szacunku \n Dawid Mądry \n Energetyka EN-2 \n Semestr 3 \n 144973")
+        print(" \n Dziękuje za użytkowanie programu. Z wyrazami szacunku \n Dawid Mądry \n Energetyka EN-2 \n Semestr 3 \n 144973")
         time.sleep(3)
         break
     elif choice == 1:
+        print("\nProgram wczytuje dane \n")
         dane = loadData('u_swietlowka_12W.csv')
+        print("Dane zostały wczytane \n")
     elif choice == 2:
+        print("\nProgram wczytuje dane \n")
         dane = loadData('u_LED_2W.csv')
+        print("Dane zostały wczytane \n")
     elif choice == 3:
         if dane == None:
-            print("Nie wczytano pliku")
+            print("Nie wczytano pliku. Wybierz [1] lub [2] przed wyświetleniem wykresu")
         else:
             plot(dane)
     elif choice == 4:
         if dane == None:
-            print("Nie wczytano pliku")
+            print("Nie wczytano pliku. Wybierz [1] lub [2] przed wyświetleniem parametrów")
         else:
             printInfo(dane)
     else:
         print("Nieprawidłowy numer")
+
+    time.sleep(1)
