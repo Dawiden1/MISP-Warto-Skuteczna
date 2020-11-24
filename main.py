b4b23f4a73bc3dd620ca2fc32fb5d1a3e0418f01
Wojciech Kucharski <wojtekkucharski105@gmail.com>
2020-11-24 13:02:12 +0100

    Moja wersja programu

---
+++ main.py
@@ -0,0 +1,76 @@
+print ("Projekt - Miernictwo i Systemy Pomiarowe")
+
+import csv #biblioteka obsługi plików csv
+import matplotlib.pyplot as plt #biblioteka rysowania wykresów funkcji
+import numpy as np #biblioteka numpy (szybkie i proste operacje na macierzach)
+import time
+
+def loadData(path):
+    data = []
+    with open(path) as csvfile:
+        readCSV = csv.reader(csvfile)
+        for row in readCSV:
+            data.append(float(row[0]))
+    return data
+
+def basicPeriod(data):
+    zeros = []
+    for i in range(len(data) - 1):
+        if data[i] * data[i + 1] < 0:
+            zeros.append(i)
+        if len(zeros) > 2:
+            break
+    return zeros[2] - zeros[0]
+
+def effectiveValue(data):
+    T = basicPeriod(data)
+    eV = 0
+    for i in range(T):
+        eV += data[i]**2
+    return (eV/T)**(1/2)
+
+def plot(data, periods = 3):
+    T = basicPeriod(data)
+    plt.plot(range(0, 3*periods*T, periods), data[:periods*T])
+    plt.ylabel("U [V]")
+    plt.xlabel("t [s]")
+    plt.show()
+
+def printInfo(data):
+    print("Największa wartość napięcia = ", max(data), " [V]")
+    print("Najmniejsza wartość napięcia = ", min(data), " [V]")
+    print("Okres podstawowy = ", basicPeriod(data)*3, " [s]")
+    print("Wartosc skuteczna = ", effectiveValue(data), " [V]")
+
+def menu():
+    print("[1] Wczytaj plik: 'u_swietlowka_12W.csv'")
+    print("[2] Wczytaj plik: 'u_LED_2W.csv'")
+    print("[3] Wyświetl wykres")
+    print("[4] Wyświetl parametry")
+    print("[0] Koniec Programu")
+    ch = int(input("Wybierz swoją opcje: "))
+    return ch
+
+dane = None
+while True:
+    choice = menu()
+    if choice == 0:
+        print("Dziękuje za użytkowanie programu. Z wyrazami szacunku \n Dawid Mądry \n Energetyka EN-2 \n Semestr 3 \n 144973")
+        time.sleep(3)
+        break
+    elif choice == 1:
+        dane = loadData('u_swietlowka_12W.csv')
+    elif choice == 2:
+        dane = loadData('u_LED_2W.csv')
+    elif choice == 3:
+        if dane == None:
+            print("Nie wczytano pliku")
+        else:
+            plot(dane)
+    elif choice == 4:
+        if dane == None:
+            print("Nie wczytano pliku")
+        else:
+            printInfo(dane)
+    else:
+        print("Nieprawidłowy numer")
